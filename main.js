1bec790732b0683f6d0982ac59c71502508247ca
will4541 <wolffbyte@gmail.com>
2024-04-28 16:23:25 -0500

    Sign up form finished with validations

---
+++ main.js
@@ -0,0 +1,20 @@
+// password and passConfirm
+
+function validatePassword()
+{
+    let pass1 = document.getElementById("pass");
+    let pass2 = document.getElementById("passConfirm");
+
+    if(pass1.value !== pass2.value)
+    {
+        let warning = document.createElement("p");
+        pass1.setCustomValidity("Passwords do not match");
+        pass2.setCustomValidity("Passwords do not match");
+        return false;
+    }
+    else
+    {
+        pass1.setCustomValidity("");
+        pass2.setCustomValidity("");
+    }
+}
